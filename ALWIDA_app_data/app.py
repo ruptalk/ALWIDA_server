a712246f7514be94d17500492e8e1a32b8f72857
hogbal <hogbal31@gmail.com>
2022-12-12 11:39:40 +0900

    add ALWIDA_app_data

---
+++ ALWIDA_app_data/app.py
@@ -0,0 +1,87 @@
+import os
+import random
+from flask import Flask, request, render_template, session, url_for, jsonify
+from sqlalchemy import func, inspect
+from models import admin_table, user_table, terminal_table, container_table, reservation_table, receipt_table, cash_table, check_table, chatting_table, message_table, init_db
+
+db = {
+    'user':'root',
+    'password':'root',
+    'host':'mariadb',
+    'port':3306,
+    'database':'alwida_db'
+}
+
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8mb4&collation=utf8mb4_general_ci"
+
+db = init_db(app)
+
+@app.route("/signin", methods=["POST"])
+def login():
+    if(request.method == "POST"):
+        id = request.form.get("id","")
+        pw = request.form.get("pw","")
+        
+        if(id != "" and pw != ""):
+            user = user_table.query.filter((user_table.id==id) & (user_table.pw==pw)).first()
+            if(hasattr(user, 'id')):
+                return jsonify({'result':True})
+            else:
+                return jsonify({'result':False})
+        else:
+            return jsonify({'result':'error'})
+        
+@app.route("/signup_id", methods=["POST"])
+def signup_id():
+    if(request.method == "POST"):
+        id = request.form.get("id","")
+        
+        if(id != ""):
+            user = user_table.query.filter(user_table.id==id).first()
+            print(user)
+            if(user == None):
+                return jsonify({'result':True})
+            else:
+                return jsonify({'result':False})
+        else:
+            return jsonify({'result':'error'})
+                
+
+@app.route("/signup_num", methods=["POST"])
+def signup_num():
+    if(request.method == "POST"):
+        id = request.form.get("id","")
+        checkNum = request.form.get("checknum","")
+        
+        user = user_table.query.filter((user_table.id==id) & (user_table.check_num==checkNum)).first()
+
+        if(hasattr(user, 'id')):
+            return jsonify({'result':True})
+        else:
+            return jsonify({'result':False})
+        
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    if(request.method == "POST"):
+        name = request.form.get("name","")
+        phoneNum = request.form.get("phoneNum","")
+        address = request.form.get("address","")
+        carNum = request.form.get("carNum","")
+        id = request.form.get("id","")
+        pw = request.form.get("pw","")
+        agreeCheck = bool(request.form.get("agreeCheck",""))
+        
+        try:
+            new_user = user_table(id=id, pw=pw, name=name, phone=phoneNum, tn="PNIT", car_num=carNum, address=address, check_num=None, info_agree=agreeCheck, info_gps=True)
+
+            db.session.add(new_user)
+            db.session.commit()
+            return jsonify({'result':True})
+        except:
+            return jsonify({'result':False})
+
+app.run(host="0.0.0.0", port=5000)
---
+++ ALWIDA_app_data/models.py
@@ -0,0 +1,103 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def init_db(app):
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    return db
+
+class admin_table(db.Model):
+    uid = db.Column(db.String(36), primary_key=True, nullable=False)
+    id = db.Column(db.String(20), nullable=False, unique=True)
+    pw = db.Column(db.String(20), nullable=False)
+    tn = db.Column(db.String(30), nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(11), nullable=False)
+    enroll = db.Column(db.Boolean, nullable=True)
+    
+    def to_json(self):
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "tn":self.tn
+        }
+
+class user_table(db.Model):
+    id = db.Column(db.String(20), primary_key=True, nullable=False)
+    pw = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(11), nullable=False)
+    tn = db.Column(db.String(30), nullable=False)
+    car_num = db.Column(db.String(20), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    check_num = db.Column(db.String(5), nullable=True)
+    info_agree = db.Column(db.Boolean, nullable=False)
+    info_gps = db.Column(db.Boolean, nullable=False)
+
+class terminal_table(db.Model):
+    tn = db.Column(db.String(30), primary_key=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(30), nullable=False)
+    car_amount = db.Column(db.Integer, nullable=False)
+    easy = db.Column(db.Integer, default=0)
+    normal = db.Column(db.Integer, default=0)
+    difficalt = db.Column(db.Integer, default=0)
+
+class container_table(db.Model):
+    container_num = db.Column(db.String(30), primary_key=True, nullable=False)
+    id = db.Column(db.String(20), nullable=False)
+    tn = db.Column(db.String(30), nullable=False)
+    scale = db.Column(db.String(30), nullable=False)
+    fm = db.Column(db.String(30), nullable=False)
+    position = db.Column(db.String(30), nullable=True)
+    contain_last_time = db.Column(db.DateTime, nullable=False)
+    in_out = db.Column(db.Boolean, nullable=False)
+
+class reservation_table(db.Model):
+    id = db.Column(db.String(20), primary_key=True, nullable=False)
+    container_num = db.Column(db.String(30), nullable=False)
+    tn = db.Column(db.String(30), nullable=False)
+    request_time = db.Column(db.DateTime, nullable=False)
+    accept_time = db.Column(db.DateTime, nullable=True)
+    suggestion = db.Column(db.String(20), nullable=True)
+
+class receipt_table(db.Model):
+    container_num = db.Column(db.String(30), primary_key=True, nullable=False)
+    id = db.Column(db.String(20), nullable=False)
+    publish = db.Column(db.Boolean, nullable=False)
+    publish_datetime = db.Column(db.DateTime, nullable=True)
+
+class cash_table(db.Model):
+    id = db.Column(db.String(20), primary_key=True, nullable=False)
+    container_num = db.Column(db.String(30), nullable=False)
+    publish_pay = db.Column(db.Boolean, nullable=False)
+    pay_datetime = db.Column(db.DateTime, nullable=True)
+
+class check_table(db.Model):
+    id = db.Column(db.String(20), primary_key=True, nullable=False)
+    request_time = db.Column(db.DateTime, nullable=False)
+    img = db.Column(db.LargeBinary, nullable=False)
+    result = db.Column(db.Integer, nullable=False)
+
+class chatting_table(db.Model):
+    id = db.Column(db.String(20), primary_key=True, nullable=False)
+    state = db.Column(db.Integer, nullable=False)
+    
+class message_table(db.Model):
+    idx = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.String(20), nullable=False)
+    message = db.Column(db.String(100), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    sender = db.Column(db.Boolean, nullable=False)
+    
+    def obj_to_dict(self):
+        return {
+            "idx":self.idx,
+            "id":self.id,
+            "message":self.message,
+            "time":self.time,
+            "sender":self.sender
+        }
+    
