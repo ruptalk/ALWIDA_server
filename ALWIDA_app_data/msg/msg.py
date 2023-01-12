3aedeccde9958993470f24eacf7d53392c93d890
hogbal <hogbal31@gmail.com>
2023-01-12 18:45:23 +0900

    add msg

--- ALWIDA_app_data/msg/msg.py
+++ ALWIDA_app_data/msg/msg.py
@@ -1,7 +1,7 @@
 import datetime
 from flask import Blueprint, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from models import admin_table, user_table, terminal_table, container_table, reservation_table, receipt_table, cash_table, check_table, chatting_table, message_table, db
+from models import terminal_table, container_table, reservation_table, chatting_table, message_table, db
 
 blue_msg = Blueprint("msg", __name__, url_prefix="/msg")
 
@@ -29,62 +29,139 @@ def info():
         else:
             return jsonify({'result':'error'})
 
-@blue_msg.route("/send", methods=["POST"])
-def send():
+@blue_msg.route("/congestion", methods=["POST"])
+def congestion():
     if(request.method == "POST"):
         id = request.form.get("id","")
-        congestion = request.form.get("congestion", type=bool)
-        numOfCar = request.form.get("numOfCar", type=bool)
-
         if(id != ""):
             try:
                 now = datetime.datetime.now()
-                if(congestion):
-                    new_msg = message_table(id=id, message="목적지 혼잡도를 알려주세요.", time=now, sender=True)
-                    db.session.add(new_msg)
-                    db.session.commit()
-                    
-                    tn = reservation_table.query.filter(reservation_table.id==id).first().tn
-                    terminal = terminal_table.query.filter(terminal_table.tn==tn).first()
-                        
-                    if(terminal.car_amount < terminal.easy):
-                        state = "원활"
-                    elif(terminal.car_amount < terminal.normal):
-                        state = "보통"
-                    else:
-                        state = "혼잡"
-                        
-                    time = now.strftime('%Y-%m-%d %H:%M')
-                    
-                    msg = f"목적지 혼잡도\n{tn} 터미널\n혼잡도: {state}\n{time} 기준"
-                elif(numOfCar):
-                    new_msg = message_table(id=id, message="부두 내 차량 현황을알려주세요.", time=now, sender=True)
-                    db.session.add(new_msg)
-                    db.session.commit()
-                        
-                    tn = reservation_table.query.filter(reservation_table.id==id).first().tn
-                    terminal = terminal_table.query.filter(terminal_table.tn==tn).first()
-                    
-                    time = now.strftime('%Y-%m-%d %H:%M')
-                    msg = f"부두 내 차량 현황\n{terminal.car_amount}\n{time}"
-                        
-                new_msg = message_table(id=id, message=msg, time=now, sender=False)
-                    
+                new_msg = message_table(id=id, message="목적지 혼잡도를 알려주세요.", time=now, sender=True)
                 db.session.add(new_msg)
                 db.session.commit()
                 
+                tn = reservation_table.query.filter(reservation_table.id==id).first().tn
+                terminal = terminal_table.query.filter(terminal_table.tn==tn).first()
+                            
+                if(terminal.car_amount < terminal.easy):
+                    state = "원활"
+                elif(terminal.car_amount < terminal.normal):
+                    state = "보통"
+                else:
+                    state = "혼잡"
+
+                time = now.strftime('%Y-%m-%d %H:%M')
+
+                msg = f"목적지 혼잡도\n{tn} 터미널\n혼잡도: {state}\n{time} 기준"
+                new_msg = message_table(id=id, message=msg, time=now, sender=False)
+                db.session.add(new_msg)
+                db.session.commit()
+                    
                 data = {
                     "description":msg,
                     "hour":now.hour,
                     "min":now.minute
                 }
-                
                 return data
             except:
                 return jsonify({'result':False})
         else:
             return jsonify({'result':'error'}) 
-        
+
+@blue_msg.route("/numOfCar", methods=["POST"])
+def numOfCar():
+    if(request.method == "POST"):
+        id = request.form.get("id","")
+        if(id != ""):
+            try:
+                now = datetime.datetime.now()
+                new_msg = message_table(id=id, message="부두 내 차량 현황을알려주세요.", time=now, sender=True)
+                db.session.add(new_msg)
+                db.session.commit()
+                            
+                tn = reservation_table.query.filter(reservation_table.id==id).first().tn
+                terminal = terminal_table.query.filter(terminal_table.tn==tn).first()
+                        
+                time = now.strftime('%Y-%m-%d %H:%M')
+                msg = f"부두 내 차량 현황\n{terminal.car_amount}\n{time}"
+                new_msg = message_table(id=id, message=msg, time=now, sender=False)
+                db.session.add(new_msg)
+                db.session.commit()
+                        
+                data = {
+                    "description":msg,
+                    "hour":now.hour,
+                    "min":now.minute
+                }
+                return data
+            except:
+                return jsonify({'result':False})
+        else:
+            return jsonify({'result':'error'}) 
+
+@blue_msg.route("/resChange", methods=["POST"])
+def resChange():
+    if(request.method == "POST"):
+        id = request.form.get("id","")
+        hour = request.form.get("hour","")
+        min = request.form.get("min","")
+        if(id != "" and hour != ""and min != ""):
+            try:
+                now = datetime.datetime.now()
+                reservation = reservation_table.query.filter(reservation_table.id==id).first()
+                time = datetime.datetime(now.year,now.month,now.day,int(hour),int(min),0)
+                reservation.accept_time = time
+                msg = f"예약 변경 요청 완료"
+                new_msg = message_table(id=id, message=msg, time=now, sender=False)
+                db.session.add(new_msg)
+                db.session.commit()
+                            
+                data = {
+                    "description":msg,
+                    "hour":now.hour,
+                    "min":now.minute
+                }
+                return data
+            except:
+                return jsonify({'result':False})
+        else:
+            return jsonify({'result':'error'}) 
+
+@blue_msg.route("/departCancle", methods=["POST"])
+def departCancle():
+    if(request.method == "POST"):
+        id = request.form.get("id","")
+        if(id != ""):
+            try:
+                now = datetime.datetime.now()
+                msg = "출발 취소 요청"
+                new_msg = message_table(id=id, message=msg, time=now, sender=True)
+                db.session.add(new_msg)
+                db.session.commit()
+                
+                return jsonify({'result':True})
+            except:
+                return jsonify({'result':False})
+        else:
+            return jsonify({'result':'error'}) 
+
+@blue_msg.route("/entryRequest", methods=["POST"])
+def entryRequest():
+    if(request.method == "POST"):
+        id = request.form.get("id","")
+        try:
+            now = datetime.datetime.now()
+            msg = "게이트 진입요청"
+            new_msg = message_table(id=id, message=msg, time=now, sender=True)
+            db.session.add(new_msg)
+            db.session.commit()
+            
+            return jsonify({'result':True})
+        except:
+            return jsonify({'result':False})
+    else:
+        return jsonify({'result':'error'}) 
+
 @blue_msg.route("/coordinate", methods=["POST"])
 def coordinate():
     if(request.method == "POST"):
