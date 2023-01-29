a0201dd665616141a5e88a4934b94f96a05b8385
hogbal <hogbal31@gmail.com>
2023-01-29 21:27:21 +0900

    modify csp

--- ALWIDA_admin/app.py
+++ ALWIDA_admin/app.py
@@ -38,7 +38,7 @@ app.register_blueprint(chatting.blue_chatting)
 
 @app.after_request
 def after_request(response):
-    response.headers["Content-Security-Policy"] = "default-src 'self' 'unsafe-inline' https://code.jquery.com/jquery-3.5.1.js https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js; img-src http://localhost:8888/ http://ec2-18-179-207-27.ap-northeast-1.compute.amazonaws.com:8888/ data:;"
+    response.headers["Content-Security-Policy"] = "default-src 'self' 'unsafe-inline' https://code.jquery.com/jquery-3.5.1.js https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js; img-src http://localhost:8888/ http://ec2-13-209-21-247.ap-northeast-2.compute.amazonaws.com:8888/ data:;"
     response.headers["X-Frame-Options"] = "deny"
     return response
 
